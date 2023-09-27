be2f71a0590060e760fea34e079c1178eb042f29
salto <salto@mail.com>
2023-09-27 06:14:22 +0000

    Linked environment ns-prod

---
+++ envs/ns-prod/static-resources/netsuite/FileCabinet/SuiteScripts/mkd/mkd_showmessage.js
@@ -0,0 +1,20 @@
+/**
+ * @NScriptType ClientScript
+ * @NApiVersion 2.x
+ */
+define([], function() {
+    function showMessage() {
+        alert('This is just a placeholder.  Edit this popup message before deploying!');
+    }
+    // this is my edit in feature_branch_1
+    // this is my edit in feature_branch_3
+    // this is my edit in feature_branch_5
+    // this is my edit in feature_branch_7
+    return {
+        pageInit: showMessage
+    }
+    // this is my edit in feature_branch_2
+    // this is my edit in feature_branch_4
+    // this is my edit in feature_branch_6
+});
+
