6f3e2736f3642860d497b0c5be3a54f07da154bc
Geoffrey Routledge <geoff@dgbi.ca>
2023-11-06 17:27:49 +0000

    Target environment fetch updates

---
+++ envs/ns-prod/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd2.js
@@ -0,0 +1,39 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/record', 'N/search'],
+
+// 20230915-191826
+  //     20230919-145638
+       // 20230925-144024
+       // 20230925-144315
+       // 20230925-144800
+       // 20230925-145202 
+       // 20230925-223906
+       // 20230925-223957
+       // geoff's changes
+       // my changes are better than your changes!!!
+/*     
+ * @param{record} record
+ * @param{search} search
+ */
+    (record, search) => {
+           
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+
+        const onRequest = (scriptContext) => { // more code from geoff
+//kgr code
+//more changes for me.
+        }
+
+        return {onRequest}
+
+    });
+// 20230913-010311 178E4999-885F-4E88-BD59-0C98EBECB0C3
